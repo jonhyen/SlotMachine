6928c5f6daf15ed7d019e709841aad095d35fa25
jonhyen <jonhyen@gmail.com>
2015-08-15 19:20:52 -0400

    finishedSlotMachineProject
    Finished project and tutorials

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Jonathan Yen on 7/26/15.
+//  Copyright (c) 2015 jonyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
